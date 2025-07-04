3c3b744fe424cc955d332cb94ea78803901926a2
Chris Teyerl <chris.teyerl@cosamey.com>
2025-07-04 09:18:35 +0200

    Install Tailwind CSS v4

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,8 @@
 /** @type {import('prettier').Config} */
 export default {
-  plugins: ['prettier-plugin-blade'],
+  plugins: ['prettier-plugin-blade', 'prettier-plugin-tailwindcss'],
   printWidth: 120,
   singleAttributePerLine: true,
   singleQuote: true,
+  tailwindStylesheet: './resources/css/app.css',
 };
